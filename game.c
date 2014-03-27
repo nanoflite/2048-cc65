9a1d74314044e135e52eedf8907018912fb92937
Johan Van den Brande <johan@vandenbrande.com>
2014-03-27 13:35:04 +0100

    up, down , left, right

--- game.c
+++ game.c
@@ -33,19 +33,6 @@ void game_init(void)
 
 void game_add_random_tile(void)
 {
-  /*
-  char y[2];
-  tile *tile;
-  static char index = 0;
-
-  y[0] = 1;
-  y[1] = 3;
-
-  tile = grid_get( 1, y[index] );
-  tile_set_value(tile, 2);
-  index++;
-  index &= 3; 
-  */
   tile *tile;
   unsigned int value;
 
@@ -73,10 +60,10 @@ bool game_is_finsihed(void)
   return ! moves_available() || won;
 }
 
-static void move(grid_get_f get_tile);
+static void move(grid_get_f get_tile, signed char row_start, signed char row_vector);
 static void move_up(void);
-// static void move_down(void);
-// static void move_right(void);
+static void move_down(void);
+static void move_right(void);
 static void move_left(void);
 
 void game_move(direction direction)
@@ -86,14 +73,12 @@ void game_move(direction direction)
     case DIR_UP:
       move_up();
       break;
-    /*
     case DIR_DOWN:
       move_down();
       break;
     case DIR_RIGHT:
       move_right();
       break;
-    */
     case DIR_LEFT:
       move_left();
       break;
@@ -112,15 +97,25 @@ static tile* get_yx(char x, char y)
 
 static void move_up(void)
 {
-  move(get_xy);
+  move(get_xy, 0, 1);
 }
 
 static void move_left(void)
 {
-  move(get_yx);
+  move(get_yx, 0, 1);
 }
 
-static void move(grid_get_f get_tile)
+static void move_down(void)
+{
+  move(get_xy, width-1, -1);
+}
+
+static void move_right(void)
+{
+  move(get_yx, width-1, -1);
+}
+
+static void move(grid_get_f get_tile, signed char row_start, signed char row_vector)
 {
   char column;
   signed char row;
@@ -129,12 +124,13 @@ static void move(grid_get_f get_tile)
   unsigned int previous_value;
   tile *previous_tile;
   tile *tile;
-
+  char i;
+  
   for(column=0;column<width;column++) {
 
-    previous_row = 0;
-
-    for(row=0;row<width;row++) {
+    row = row_start;
+    previous_row = row_start;
+    for(i=0;i<width;i++) {
 
       tile = get_tile( column, row );
       value = tile_get_value(tile);
@@ -152,8 +148,8 @@ static void move(grid_get_f get_tile)
         } else {
           if ( row != previous_row ) {
             if ( 0 != previous_value ) {
-              previous_tile = get_tile( column, previous_row + 1 );
-              previous_row++; 
+              previous_row += row_vector; 
+              previous_tile = get_tile( column, previous_row );
             }
             tile_set_value(tile, 0);
             tile_set_value(previous_tile, value);
@@ -161,7 +157,10 @@ static void move(grid_get_f get_tile)
           }
         }
       }
+      
+      row += row_vector;
     }
+
   }
 }
 
@@ -172,28 +171,6 @@ void game_draw(void)
 
 static void insert_start_tiles(void)
 {
-  tile *tile;
-
-  // 0123
-  // _2_2
-  tile = grid_get(0, 1);
-  tile_set_value(tile, 2); 
-
-  tile = grid_get(0, 3);
-  tile_set_value(tile, 2); 
-
-  // 0123
-  // 422_
-  tile = grid_get(1, 0);
-  tile_set_value(tile, 4); 
-
-  tile = grid_get(1, 1);
-  tile_set_value(tile, 2); 
-
-  tile = grid_get(1, 2);
-  tile_set_value(tile, 2); 
-
-  /*
   unsigned char i;
   tile *tile;
 
@@ -201,7 +178,6 @@ static void insert_start_tiles(void)
     tile = grid_random_available_cell();
     tile_set_value(tile, 2); 
   }
-  */
 }
 
 static unsigned int get_random_value(void)
